e35c78a6e2be12fae5301f9c7551b2203282ae8a
Jai Parera <67336232+NFJ1618@users.noreply.github.com>
2024-02-28 10:42:14 -0800

    add UI for fuel tracker and code

--- Assets/Scripts/UI_FuelTracker.cs
+++ Assets/Scripts/UI_FuelTracker.cs
@@ -8,15 +8,26 @@ using UnityEngine.UI;
 public class UI_FuelTracker : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] Slider slider;
+    private float fuel;
     void Start()
     {
-        
+        fuel = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetFuel(GetFuel()-1);
+        slider.value = fuel;
+    }
+
+    public void SetFuel(float amt) {
+        fuel = amt;
+    }
+
+    public float GetFuel() {
+        return fuel;
     }
 
     // code for a health bar, similar to a fuel tracker
