2fdd825083d3b2a6baa6889ef129e40cd1f3e129
skylarshi123 <skylarshi123@gmail.com>
2024-03-06 13:19:23 -0800

    one more scene to go and index scene,

--- Assets/Scripts/Failed.cs
+++ Assets/Scripts/Failed.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Include the UI namespace to work with UI elements.
 
-public class Failed : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // You can attach these buttons in the inspector by dragging and dropping.
+
+    [SerializeField] private Button restartButton;
+    [SerializeField] private Button endGameButton;
+    [SerializeField] private Button nextLevelButton;
+
+
+    private void Start()
     {
-        
+        // Add listeners for button clicks.
+        restartButton.onClick.AddListener(RestartLevel);
+        endGameButton.onClick.AddListener(EndGame);
+        nextLevelButton.onClick.AddListener(NextLevel);
     }
 
-    // Update is called once per frame
-    void Update()
+    void RestartLevel()
     {
-        
+        // Reloads the current scene.
+        SceneManager.LoadScene("Level0");
+
+    }
+
+    void EndGame()
+    {
+        // Quits the application.
+        // Note: This will only work in a built game, not in the Unity Editor.
+        Application.Quit();
+    }
+
+    void NextLevel()
+    {
+        // Loads the next level based on the current scene's build index.
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Check if the next scene index exceeds the number of scenes in Build Settings.
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("Last level reached. No more levels to load.");
+            // Optionally, loop back to the first level or show a game over screen.
+        }
     }
 }
